a82e0e9bd9350fb09ff210302e5e37e8d27ddc0f
sick <andie_wand@gmx.de>
2020-12-06 17:51:52 +0100

    add setter to gatewayanswer for safer usage

--- src/lib/GatewayAnswer.ts
+++ src/lib/GatewayAnswer.ts
@@ -2,4 +2,23 @@ export class GatewayAnswer {
   history: string[];
   content?: string;
   error?: string;
+  public setContent(content: string){
+    this.content = content;
+    return this;
+  }
+  public setError(error: string){
+    this.error = error;
+    return this;
+  }
+  public setHistory(history: string[]){
+    this.history = history;
+    return this;
+  }
+  public addHistory(name: string){
+    if(!this.history){
+      this.history = [];
+    }
+    this.history.push(name);
+    return this;
+  }
 }
